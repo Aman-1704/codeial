96edb66470136e3ad8e703d1a5d389722a9e1ee0
Aman <amansingh60046@gmail.com>
2023-08-23 19:19:47 +0530

    Second Commit: Add a controller and a router for home

---
+++ controllers/home_controller.js
@@ -0,0 +1,7 @@
+module.exports.home = function (req, res) {
+    return res.end('<h1> Express is up for codial!</h1>');
+}
+
+// module.exports.new = function (req, res) {
+//     return res.end('<h1> Hello codial users </h1>');
+// }
---
+++ routes/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+const homeController = require('../controllers/home_controller');
+
+console.log('router loded');
+
+router.get('/', homeController.home);
+
+// router.get('/profile', homeController.new);
+
+module.exports = router;
