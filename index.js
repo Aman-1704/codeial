b385c28161574ba3da1e9c7779dcd8180d0cc72b
Aman <amansingh60046@gmail.com>
2023-08-24 13:12:24 +0530

    Added layout and sub pages

--- index.js
+++ index.js
@@ -2,8 +2,18 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
-const expresslayout = require('express-ejs-layouts');
-app.use(expresslayout);
+
+const expressLayouts = require('express-ejs-layouts');
+
+app.use(express.static('./assets'));
+
+//  use express Layouts
+app.use(expressLayouts);
+
+// extract script and style from sub pages into the layout
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
+
 // use express router
 app.use('/', require('./routes'));
 
