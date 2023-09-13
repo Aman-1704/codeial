557f65a335f7d6ba68d64775bb93c6d83ab84a4e
Aman <amansingh60046@gmail.com>
2023-09-13 17:48:39 +0530

    Display Posts and related User

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,10 +1,19 @@
-module.exports.home = function (req, res) {
-    console.log(req.cookies);
-    res.cookie('user_id', 25);
-    return res.render('home',{
-        title:"Home"
-    });
+const Post = require('../models/post');
+
+module.exports.home = async function home(req, res) {
+    try {
+      let posts = await Post.find({}).populate('user');
+      return res.render('home',{
+        title:"Codeial | Home",
+        posts: posts
+      });
+    } catch (err) {
+      console.error(err);
+      return res.status(500).send("Something went wrong");
+    }
 }
+  
+  
 
 // module.exports.new = function (req, res) {
 //     return res.end('<h1> Hello codial users </h1>');
