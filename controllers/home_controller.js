99a4008d168c940020ffcfe2f82e0b8b2750bd43
Aman <amansingh60046@gmail.com>
2023-09-14 20:47:52 +0530

    display comment and realatedd user

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,20 +1,62 @@
-const Post = require('../models/post');
+const Post=require('../models/post');
+const User=require('../models/user');
 
-module.exports.home = async function home(req, res) {
-    try {
-      let posts = await Post.find({}).populate('user');
-      return res.render('home',{
-        title:"Codeial | Home",
-        posts: posts
-      });
-    } catch (err) {
-      console.error(err);
-      return res.status(500).send("Something went wrong");
+
+
+
+
+module.exports.home = async function(req,res){
+
+    // console.log(req.cookies);
+    // res.cookie("user_iddd","12344455")
+
+    // Post.find({},function(err,posts){
+    //     if(err){
+    //         console.log("Error in fetching all the posts from the database");
+    //         return;
+    //     }
+    //     return  res.render('home',{
+    //         title:"Home_Page",
+    //         posts:posts
+    //     });
+    // })
+
+
+    // populating the user of each post excluding the password field
+    // and also sorting in reverse chronological order
+
+
+    try{
+
+        let posts= await Post.find({})
+        .populate('user',"-password")
+        .populate({
+            path:'comments',
+            populate:{
+                path:'user',
+                // to hide as many fields as I want from being populated
+                // select:'-name -password -email',
+                // to only populate the name field of the user
+                select:'name',
+            },
+            options: {
+                sort: { createdAt: -1 } // sort by createdAt field in descending order
+            }
+        })
+        // below will also work
+        // .sort("-createdAt")
+        .sort({"createdAt":-1});
+
+        let users = await User.find({});
+
+        return  res.render('home',{
+            title:"Codeial | Home",
+            posts:posts,
+            all_users:users   
+        });
+    }
+    catch(err){
+        console.log("Error",err);
+        return;
     }
 }
-  
-  
-
-// module.exports.new = function (req, res) {
-//     return res.end('<h1> Hello codial users </h1>');
-// }
